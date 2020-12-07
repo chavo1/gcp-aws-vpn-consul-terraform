4e3c8e88a6d5c775cf2513b92793ff9e0ac18ba6
chavdar <chavdar.rakov@gmail.com>
2020-12-07 16:58:17 +0200

    Update gcp_vpc.tf

--- gcp_vpc.tf
+++ gcp_vpc.tf
@@ -4,7 +4,6 @@ module "gcp_vpc_virginia" {
   region                  = var.region_gcp
   region_zone             = var.region_zone
   project_name            = var.project_name
-  credentials_file_path   = var.credentials_file_path
   remote_cidr             = var.remote_cidr
   fw_source_range         = var.fw_source_range
   subnet_range            = var.subnet_range
