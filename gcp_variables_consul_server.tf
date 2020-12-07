8f7168974aff2a031ae39554b80b0a2883af0642
chavdar <chavdar.rakov@gmail.com>
2020-12-07 17:14:27 +0200

    Update gcp_variables_consul_server.tf

--- gcp_variables_consul_server.tf
+++ gcp_variables_consul_server.tf
@@ -2,9 +2,7 @@ variable "server_count_gcp" {}
 variable "dc_net_gcp" {
   default = 32
 }
-variable "public_key_path" {
-  description = "Path to SSH public key to be attached to cloud instances"
-}
+
 variable "gcp_image" {
   description = "GCP Image with consul installed"
 }
